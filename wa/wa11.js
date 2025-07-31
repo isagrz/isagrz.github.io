57a8fb393e7c67fe2d2f4d5f4b9e11b043eb2e4d
isagrz <isagrznar@gmail.com>
2025-07-30 19:21:24 -0600

    Create wa11.js

---
+++ wa/wa11.js
@@ -0,0 +1,24 @@
+const displayedImage = document.getElementById('fullImage');
+const thumbBar = document.querySelector('.thumb-bar');
+
+// Array of image filenames and alt text
+const images = [
+  { src: 'img/photobooth.jpg', alt: 'Photo booth moment' },
+  { src: 'img/out-to-nyc.jpg', alt: 'Out to NYC' },
+  { src: 'img/anxiety-graphic.jpg', alt: 'Funny anxiety graphic' },
+  { src: 'img/gingham-bedding.jpg', alt: 'Cute gingham bedding' },
+  { src: 'img/whats-in-my-bag.jpg', alt: 'Bag contents' },
+  { src: 'img/strawberry-heels.jpg', alt: 'Strawberry heels' }
+];
+
+// Loop through images and create thumbnail elements
+images.forEach(image => {
+  const newImage = document.createElement('img');
+  newImage.src = image.src;
+  newImage.alt = image.alt;
+  newImage.addEventListener('click', () => {
+    displayedImage.src = image.src;
+    displayedImage.alt = image.alt;
+  });
+  thumbBar.appendChild(newImage);
+});
