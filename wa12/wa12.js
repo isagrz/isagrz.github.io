5fd1977c73d42ee3ebdf5dad23df6dedf1acb1c6
isagrz <isagrznar@gmail.com>
2025-08-01 02:44:13 -0600

    Update wa12.js

--- wa12/wa12.js
+++ wa12/wa12.js
@@ -4,18 +4,14 @@ const quoteAuthor = document.querySelector('#js-quote-author');
 const tweetButton = document.querySelector('#js-tweet');
 
 const endpoint = 'https://api.quotable.io/random';
-const proxyURL = `https://api.allorigins.win/get?url=${encodeURIComponent(endpoint)}`;
 
 async function getQuote() {
   showLoading();
   try {
-    const response = await fetch(proxyURL);
+    const response = await fetch(endpoint);
     if (!response.ok) throw Error(response.statusText);
-    
     const data = await response.json();
-    const quoteData = JSON.parse(data.contents);
-
-    displayQuote(quoteData);
+    displayQuote(data);
   } catch (err) {
     console.error(err);
     alert('Failed to fetch quote.');
